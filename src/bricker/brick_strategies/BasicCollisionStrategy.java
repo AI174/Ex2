32d4e087b56c3e3ebc89f81303f809009839c44d
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-02-17 00:08:53 +0200

    BasicCollisionStrategy documented

--- src/bricker/brick_strategies/BasicCollisionStrategy.java
+++ src/bricker/brick_strategies/BasicCollisionStrategy.java
@@ -1,3 +1,8 @@
+/**
+ * Represents a basic collision strategy used for handling collisions between game objects.
+ * @authors adan.ir1, hayanat2002
+ * @see CollisionStrategy
+ */
 package bricker.brick_strategies;
 
 import danogl.GameObject;
@@ -8,12 +13,23 @@ import danogl.util.Counter;
 public class BasicCollisionStrategy implements CollisionStrategy{
     private final GameObjectCollection gameObjectCollection;
     private final Counter currBricksNumber;
-
+    /**
+     * Constructs a new BasicCollisionStrategy with the specified game object collection
+     * and current bricks counter.
+     * @param gameObjectCollection The collection of game objects.
+     * @param currBricksNumber The counter tracking the current number of bricks.
+     */
     public BasicCollisionStrategy(GameObjectCollection gameObjectCollection, Counter currBricksNumber) {
         this.gameObjectCollection = gameObjectCollection;
         this.currBricksNumber = currBricksNumber;
     }
-
+    /**
+     * Handles the collision between two game objects.
+     * If the removing the thisObj(brick) is successful,
+     * the current bricks counter is decremented accordingly.
+     * @param thisObj The first game object involved in the collision.
+     * @param otherObj The second game object involved in the collision.
+     */
     @Override
     public void onCollision(GameObject thisObj, GameObject otherObj) {
         if(this.gameObjectCollection.removeGameObject(thisObj, Layer.STATIC_OBJECTS)){
