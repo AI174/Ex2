4b480a92b11916f6ff7c1b3881ea665175b2d546
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-02-17 00:13:37 +0200

    CollisionStrategy documented

--- src/bricker/brick_strategies/CollisionStrategy.java
+++ src/bricker/brick_strategies/CollisionStrategy.java
@@ -1,7 +1,17 @@
+/**
+ * Defines the contract for collision strategies used for handling collisions
+ * between game objects.
+ * @authors adan.ir1, hayanat2002
+ */
 package bricker.brick_strategies;
 
 import danogl.GameObject;
 
 public interface CollisionStrategy {
+    /**
+     * Handles the collision between two game objects.
+     * @param thisObj The first game object involved in the collision.
+     * @param otherObj The second game object involved in the collision.
+     */
     void onCollision(GameObject thisObj, GameObject otherObj);
 }
